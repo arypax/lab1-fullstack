2537897db5b19f576b5eac4dc5c7bf98de7c42b9
arypax <vlad.pineker@gmail.com>
2025-12-07 21:21:22 +0500

    Fix Next.js config for monorepo and use local next

--- apps/frontend/next.config.js
+++ apps/frontend/next.config.js
@@ -1,9 +1,8 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
-  experimental: {
-    typedRoutes: true,
-  },
+  typedRoutes: true,
+  output: 'standalone',
 };
 
 module.exports = nextConfig;
